eef0ff88d80f7ec854385aef385ae932d1bc3a4d
losfair <zhy20000919@hotmail.com>
2024-05-13 00:29:10 +0800

    ci fix & test

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,7 +1,16 @@
 import test from 'ava'
+import * as helios from "../index.js"
 
-import { sum } from '../index.js'
+const config = new helios.HeliosClientConfig();
+config.checkpoint = "0xf38911c84ca0d777fba2732fdc9548e0dd33fa48e67f45378d30290c69521ad4";
+const client = helios.HeliosClient.withConfig(config);
 
-test('sum from native', (t) => {
-  t.is(sum(1, 2), 3)
-})
+await client.waitSynced();
+
+test("vitalik.eth balance", async (t) => {
+  const balance = await client.getBalance("0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045", helios.BlockTag.finalized())
+  console.log("balance: " + balance);
+
+  t.assert(typeof balance === "bigint");
+  t.assert(balance > 0n);
+});
