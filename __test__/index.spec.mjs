8c874c1757b41ce8ba3b1db063fd283844011190
losfair <zhy20000919@hotmail.com>
2024-05-13 04:08:09 +0800

    0.1.0

--- __test__/index.spec.mjs
+++ __test__/index.spec.mjs
@@ -1,16 +1,59 @@
 import test from 'ava'
 import * as helios from "../index.js"
+import * as ethers from "ethers"
+import * as fs from "fs/promises"
+import { namehash } from '@ensdomains/ensjs/utils';
+
+const vitalikAddress = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+
+const { data: [{ blockroot, slot }] } = await (await fetch("https://beaconcha.in/api/v1/epoch/finalized/slots")).json();
+if (typeof blockroot !== "string") throw new Error("Invalid blockroot");
+console.log(`using blockroot ${blockroot} at slot ${slot}`);
 
 const config = new helios.HeliosClientConfig();
-config.checkpoint = "0xf38911c84ca0d777fba2732fdc9548e0dd33fa48e67f45378d30290c69521ad4";
+config.checkpoint = blockroot;
+
+// Test environment (Docker node:*-slim) doesn't like TLS?
+// rpc error on method: get_proof, message: error sending request for url (https://rpc.flashbots.net/): error trying to connect: unexpected EOF (unable to get local issuer certificate)
+config.consensusRpc = "http://unstable.mainnet.beacon-api.nimbus.team";
+config.executionRpc = "http://cloudflare-eth.com";
+
 const client = helios.HeliosClient.withConfig(config);
 
 await client.waitSynced();
 
+test("chain id", async (t) => {
+  const chainId = await client.chainId();
+  t.deepEqual(chainId, 1n);
+});
+
+test("get block", async (t) => {
+  const block = JSON.parse(await client.getBlockByNumber(helios.BlockTag.latest(), false))
+  const timestamp = ethers.getBigInt(block.timestamp);
+  const now = Math.floor(Date.now() / 1000);
+  t.assert(timestamp > now - 300 && timestamp < now + 300);
+});
+
 test("vitalik.eth balance", async (t) => {
-  const balance = await client.getBalance("0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045", helios.BlockTag.finalized())
+  const balance = await client.getBalance(vitalikAddress, helios.BlockTag.finalized())
   console.log("balance: " + balance);
 
   t.assert(typeof balance === "bigint");
   t.assert(balance > 0n);
+});
+
+test("vitalik.eth ens resolve", async (t) => {
+  const registry = new ethers.Interface(
+    await fs.readFile(new URL("./abi/ens_registry.json", import.meta.url), "utf-8"),
+  );
+  let call = new helios.CallOpts();
+  call.setTo("0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e");
+  call.setData(ethers.getBytes(registry.encodeFunctionData("resolver", [namehash("vitalik.eth")])));
+  const resolverAddress = await client.call(call, helios.BlockTag.latest())
+  const resolver = new ethers.Interface(await fs.readFile(new URL("./abi/ens_public_resolver_2.json", import.meta.url), "utf-8"));
+  call = new helios.CallOpts();
+  call.setTo(resolverAddress.subarray(12, 32));
+  call.setData(ethers.getBytes(resolver.encodeFunctionData("addr(bytes32)", [namehash("vitalik.eth")])))
+  const outputAddress = await client.call(call, helios.BlockTag.latest())
+  t.deepEqual(ethers.hexlify(outputAddress.subarray(12, 32)), ethers.hexlify(vitalikAddress));
 });
--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,13 @@ mod wrappers;
 
 use std::{path::PathBuf, str::FromStr};
 
+use ethers::types::U256;
 use helios_client::ClientBuilder;
 use helios_config::Network;
-use wrappers::{EthAddress, EthU256Output, JsBlockTag, NapiEyreReport, SomeDB};
+use napi::bindgen_prelude::Buffer;
+use wrappers::{
+  EthAddress, EthU256Output, JsBlockTag, JsCallOpts, NapiAnyhowError, NapiEyreReport, SomeDB,
+};
 
 #[macro_use]
 extern crate napi_derive;
@@ -63,6 +67,11 @@ impl JsHeliosClient {
     Ok(Self { inner: client })
   }
 
+  #[napi]
+  pub async fn wait_synced(&self) {
+    self.inner.wait_synced().await;
+  }
+
   #[napi(
     ts_args_type = "address: string | Uint8Array, block_tag: BlockTag",
     ts_return_type = "Promise<bigint>"
@@ -80,8 +89,90 @@ impl JsHeliosClient {
     Ok(EthU256Output(output))
   }
 
+  #[napi(ts_args_type = "address: string | Uint8Array, block_tag: BlockTag")]
+  pub async fn get_code(
+    &self,
+    address: EthAddress,
+    block_tag: &JsBlockTag,
+  ) -> napi::Result<Buffer> {
+    let output = self
+      .inner
+      .get_code(&address.0, block_tag.0)
+      .await
+      .map_err(NapiEyreReport)?;
+    Ok(Buffer::from(output))
+  }
+
+  #[napi(
+    ts_args_type = "address: string | Uint8Array, block_tag: BlockTag",
+    ts_return_type = "Promise<bigint>"
+  )]
+  pub async fn get_nonce(
+    &self,
+    address: EthAddress,
+    block_tag: &JsBlockTag,
+  ) -> napi::Result<EthU256Output> {
+    let output = self
+      .inner
+      .get_nonce(&address.0, block_tag.0)
+      .await
+      .map_err(NapiEyreReport)?;
+    Ok(EthU256Output(U256::from(output)))
+  }
+
   #[napi]
-  pub async fn wait_synced(&self) {
-    self.inner.wait_synced().await;
+  pub async fn call(&self, call_opts: &JsCallOpts, block_tag: &JsBlockTag) -> napi::Result<Buffer> {
+    let call_opts = (*call_opts.0.lock().unwrap()).clone();
+    let output = self
+      .inner
+      .call(&call_opts, block_tag.0.clone())
+      .await
+      .map_err(NapiEyreReport)?;
+    Ok(Buffer::from(output))
+  }
+
+  #[napi(ts_return_type = "Promise<bigint>")]
+  pub async fn estimate_gas(&self, call_opts: &JsCallOpts) -> napi::Result<EthU256Output> {
+    let call_opts = (*call_opts.0.lock().unwrap()).clone();
+    let output = self
+      .inner
+      .estimate_gas(&call_opts)
+      .await
+      .map_err(NapiEyreReport)?;
+    Ok(EthU256Output(U256::from(output)))
+  }
+
+  #[napi(ts_return_type = "Promise<bigint>")]
+  pub async fn get_block_number(&self) -> napi::Result<EthU256Output> {
+    let output = self
+      .inner
+      .get_block_number()
+      .await
+      .map_err(NapiEyreReport)?;
+    Ok(EthU256Output(U256::from(output)))
+  }
+
+  #[napi]
+  pub async fn get_block_by_number(
+    &self,
+    block_tag: &JsBlockTag,
+    full_tx: bool,
+  ) -> napi::Result<String> {
+    let output = self
+      .inner
+      .get_block_by_number(block_tag.0, full_tx)
+      .await
+      .map_err(NapiEyreReport)?;
+    Ok(
+      serde_json::to_string(&output)
+        .map_err(anyhow::Error::from)
+        .map_err(NapiAnyhowError)?,
+    )
+  }
+
+  #[napi(ts_return_type = "Promise<bigint>")]
+  pub async fn chain_id(&self) -> EthU256Output {
+    let output = self.inner.chain_id().await;
+    EthU256Output(U256::from(output))
   }
 }
